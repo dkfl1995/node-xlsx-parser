b932b3f753b421f0fc851b83beb26311b1424cf9
Vlad Vytiaz <vlaio.95@gmail.com>
2021-05-11 14:10:43 +0300

    Small syntax fixes, revised the tool initialization

--- libs/index.js
+++ libs/index.js
@@ -3,22 +3,21 @@ const xlsx = require('xlsx');
 ////////
 
 module.exports = class XLSXParser {
-  _config = null;
+  _config = {};
   _defaultOutput = {
     result: null,
     error: null
   }
 
   constructor(config) {
-    this._config = config;
-    if (typeof this._config !== 'object') {
-      throw `Please, add correct "config" for required fields`;
+    this._config = {
+      ...this._config,
+      ...config
     }
-    if (!this._config.input) throw 'You miss a input file';
   }
 
-  xlsx_to_csv() {
-    const _worksheet = this._load_xlsx();
+  xlsx_to_csv(file, options) {
+    const _worksheet = this._load_xlsx(file);
     const csv = xlsx.utils.sheet_to_csv(_worksheet);
     
     if (!csv) {
@@ -34,15 +33,15 @@ module.exports = class XLSXParser {
     }
   }
 
-  xlsx_to_json() {
-    const _worksheet = this._load_xlsx();
+  xlsx_to_json(file, options) {
+    const _worksheet = this._load_xlsx(file);
     const json = xlsx.utils.sheet_to_json(_worksheet, {
       defval: null,
       blankrows: false,
       raw: false
     });
     
-    if (!csv) {
+    if (!json) {
       return {
         ...this._defaultOutput,
         error: "failed to parse xlsx as json",
@@ -55,8 +54,7 @@ module.exports = class XLSXParser {
     };
   }
 
-  _load_xlsx() {
-    const { input } = this._config;
+  _load_xlsx(input) {
     if (!input) throw 'Input file is not defined!';
 
     const _xlsxFileInfo = xlsx.readFile(input);
